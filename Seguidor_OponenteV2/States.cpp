be6984b8b62f5dfa8c7dce96d431a7dfcf56150a
Sanie Peralta <sanie.peralta@fiuna.edu.py>
2025-09-28 12:19:39 -0300

    Add filter
    Filtro de lectura

--- Seguidor_OponenteV2/States.cpp
+++ Seguidor_OponenteV2/States.cpp
@@ -1,23 +1,27 @@
 #include "States.h" // Incluye la cabecera de clase OS
 #include <xmotionV3.h> // Incluye la libreria xmotionV3
 
-//////////////////////////  VARIABLES AUXILIARES  //////////////////////////
+        //// Auxiliar Sensor Oponente:
+    // Filtro:
+    const int FILTER_N = 3;
+    int L_OS_F;
+    int LD_OS_F;
+    int C_OS_F;
+    int RD_OS_F;
+    int R_OS_F;
+    int L_LS_F;
+    int R_LS_F;
 
-// Velocidades:
-int max_speed = 70;   //velocidad maxima
-int fast_speed = 50;   // velocidad para el frente
-int mean_speed = 20;   // velocidad promedio de busqueda
-int speed;
-int left_speed;
-int rigth_speed;
-
-// Tiempo
-unsigned long duracionBusqueda = 1000; 
-unsigned long duracionGiro90 = 200; 
-unsigned long duracionGiro180 = duracionGiro90 *2; 
-unsigned long tiempoRetrocesoInicio;
-unsigned long tiempoGiro180Inicio;
-unsigned long duracionRetroceso = 500; // 500 ms, lo mismo que el delay original
+// --- Funcion filtro por mayoria ---
+template<typename T>
+bool filtro(T &sensor, int n = FILTER_N){
+  int suma = 0;
+  for(int i=0; i<n; i++){
+    suma += sensor.lectura() ? 1 : 0;
+    delayMicroseconds(200); // opcional, pequeño retardo
+  }
+  return (suma > n/2);
+}
 
 // Constructor de la clase States.
 States::States(OS& L_OS, OS& LD_OS, OS& C_OS, OS& RD_OS, OS& R_OS, LS& L_LS, LS& R_LS)
@@ -35,9 +39,22 @@ void States::begin() {
 }
 
 void States::update() {
-    // Lee los sensores
-    bool Read_OS[] = {L_OS.lectura(), LD_OS.lectura(), C_OS.lectura(), RD_OS.lectura(), R_OS.lectura()};
-    bool Read_LS[] = {L_LS.lectura(), R_LS.lectura()};
+    // Leemos sensores con filtro
+    L_OS_F = filtro(L_OS);
+    LD_OS_F = filtro(LD_OS);
+    C_OS_F = filtro(C_OS);
+    RD_OS_F = filtro(RD_OS);
+    R_OS_F = filtro(R_OS);
+    L_LS_F = filtro(L_LS);
+    R_LS_F = filtro(R_LS);
+
+    // Ubicamos esas lecturas dentro de vectores
+    bool Read_OS[] = {
+      L_OS_F, LD_OS_F, C_OS_F, RD_OS_F, R_OS_F
+    };
+    bool Read_LS[] = {
+      L_LS_F, R_LS_F
+    };
 
     //////////////////////////////////  CONDICIONALES  ////////////////////////////////
     bool oponente = (Read_OS[1] || Read_OS[2] || Read_OS[3] || Read_OS[0] || Read_OS[4]);
@@ -87,9 +104,10 @@ void States::update() {
         case BUSCAR:
             if (millis() - tiempoBusquedaInicio < duracionBusqueda) {
                 xmotion.MotorControl(mean_speed, mean_speed); //Avanza para escanear
-            } else if (millis() - tiempoBusquedaInicio < 1.25*duracionBusqueda) {
-                xmotion.MotorControl(mean_speed, -mean_speed); //Gira para escanear
-            } else {
+            } //else if (millis() - tiempoBusquedaInicio < 1.25*duracionBusqueda) {
+                //xmotion.MotorControl(mean_speed, -mean_speed); //Gira para escanear
+            //} 
+            else {
                 tiempoBusquedaInicio = millis(); // Reiniciar el temporizador de búsqueda
             }
             
@@ -186,5 +204,4 @@ void States::update() {
         break;
     }
 }
-
 
--- Seguidor_OponenteV2/States.h
+++ Seguidor_OponenteV2/States.h
@@ -34,9 +34,25 @@ private:
     OS& R_OS; 
     LS& L_LS; 
     LS& R_LS; 
+
+        // --- Logica de estados ---
     Estado estadoActual;
     unsigned long tiempoBusquedaInicio;
-    const unsigned long duracionBusqueda = 1000;   
+    unsigned long tiempoRetrocesoInicio;
+    unsigned long tiempoGiro180Inicio;
+
+    // --- Parametros de comportamiento
+    const int max_speed = 70;
+    const int fast_speed = 50;
+    const int mean_speed = 20;
+    int left_speed;
+    int rigth_speed;
+
+    const unsigned long duracionBusqueda = 1000; 
+    const unsigned long duracionGiro90 = 200; 
+    const unsigned long duracionGiro180 = duracionGiro90 * 2; 
+    const unsigned long duracionRetroceso = 500;
+
 };
 
 #endif
